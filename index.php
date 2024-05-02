641a6ba7e32d88611bb67938dcaa9a4a6f058976
xeeshaunali <66247734+xeeshaunali@users.noreply.github.com>
2024-05-02 14:28:47 +0500

    Working on Employee Management System

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="app.css">
     <link rel="stylesheet" href="./css/bootstrap.css">   
     <script src="datatable/jquery-3.7.0.js"></script>    
     <title>|| EMPLOYEE MANAGEMENT SYSTEM ||</title>
